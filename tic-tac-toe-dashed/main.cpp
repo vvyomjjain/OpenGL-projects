83744fa3a8130db089748357166f3cef2c436b88
vvyomjjain <vvyomjjain@gmail.com>
2018-03-11 13:21:20 +0530

    Learning OpenGL

---
+++ tic-tac-toe-dashed/main.cpp
@@ -0,0 +1,53 @@
+#include<gl/gl.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void init(void){
+    glClearColor(1.0,1.0,1.0,0.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0,200.0,0.0,150.0);
+}
+
+void lineSegment(void){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0,0.4,0.2);
+    glLineWidth(5.0);
+    glLineStipple(1, 0xAAAA);
+    glEnable(GL_LINE_STIPPLE);
+
+    glBegin(GL_LINES);
+        glVertex2i(60,20);
+        glVertex2i(60,120);
+    glEnd();
+
+    glBegin(GL_LINES);
+        glVertex2i(100,20);
+        glVertex2i(100,120);
+    glEnd();
+
+    glBegin(GL_LINES);
+        glVertex2i(30,90);
+        glVertex2i(130,90);
+    glEnd();
+
+    glBegin(GL_LINES);
+        glVertex2i(30,50);
+        glVertex2i(130,50);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(50,100);
+    glutInitWindowSize(800,450);
+    glutCreateWindow("A simple line");
+    init();
+    glutDisplayFunc(lineSegment);
+    glutMainLoop();
+
+    return 0;
+}
---
+++ types-of-triangle/main.cpp
@@ -0,0 +1,50 @@
+#include<gl/gl.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void init(void){
+    glClearColor(1.0,1.0,1.0,0.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0,800.0,0.0,450.0);
+}
+
+void lineSegment(void){
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(0.0,0.4,0.2);
+        glVertex2i(200, 100);
+        glVertex2i(400, 100);
+        glVertex2i(300, 400);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(0.0,0.2,0.2);
+        glVertex2i(40, 40);
+        glVertex2i(140, 40);
+        glVertex2i(40, 140);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(0.4,0.0,0.2);
+        glVertex2i(500, 150);
+        glVertex2i(700, 150);
+        glVertex2i(600, 323);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(50,100);
+    glutInitWindowSize(800,450);
+    glutCreateWindow("A simple line");
+    init();
+    glutDisplayFunc(lineSegment);
+    glutMainLoop();
+
+    return 0;
+}
